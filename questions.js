577860416dcf11718ee4afc8ba2ef99d44c28a45
Ruhaim <ruhaim860@gmail.com>
2024-04-04 10:24:52 +0530

    Send questions

---
+++ questions.js
@@ -0,0 +1,9 @@
+const questions = [
+  "Hi Ruhaim, thanks for your recent order! your order has beed delivered to your location. Would you mind taking a moment to share your feedback?",
+  "On a scale of 1 to 5, how would you rate the overall quality of the Cake you purchased from us?",
+  "How satisfied were you with the taste of the Cake? (1 - Not satisfied, 5 - Very satisfied)",
+  "How would you describe your experience with the Cake in a few words?",
+  "Could you please share a few lines about your overall experience with our bakery and the product?",
+];
+
+module.exports = questions;
